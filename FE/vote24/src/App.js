1e71704f42fe81e2d87180b149a090a6248b92d4
j1h00 <guruboy@snu.ac.kr>
2022-01-17 14:47:13 +0900

    feat: Done main_landing_page

--- FE/vote24/src/App.js
+++ FE/vote24/src/App.js
@@ -1,18 +1,18 @@
 import { useState } from "react";
 import "./App.css";
-import Main from "./components/Main";
+import Main from "./pages/Main";
 import Navbar from "./components/Navbar";
 import NavbarOnLogin from "./components/NavbarOnLogin";
 import Footer from "./components/Footer";
 
 function App() {
-  const [isLogIn, setIsLogIn] = useState(true);
+  const [isLogIn, setIsLogIn] = useState(false);
   return (
     <div>
       <div className="navbar">
         {isLogIn ? <Navbar></Navbar> : <NavbarOnLogin></NavbarOnLogin>}
       </div>
-      <div className="body">
+      <div className="page_body">
         <Main></Main>
       </div>
       <Footer />
