8e2a7c2cb6242c76889d7dbc7130fa42f1696ae9
j1h00 <guruboy@snu.ac.kr>
2022-01-17 14:10:35 +0900

    feat: Add main_landing_page logo

--- FE/vote24/src/components/Main.js
+++ FE/vote24/src/components/Main.js
@@ -1,9 +1,15 @@
 import React from "react";
+import "./css/main.css";
+import header_image from "../images/header_image.png";
 
 function Main() {
   return (
     <div>
-      <div class="container bg-primary"></div>
+      <div className="body bg-primary d-flex justify-content-center align-items-center">
+        <div className="container bg-yellow">
+          <img className="absolute header_image" src={header_image}></img>
+        </div>
+      </div>
     </div>
   );
 }
