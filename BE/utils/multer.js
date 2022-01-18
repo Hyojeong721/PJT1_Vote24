a5a2c01aca9ecec813dc020fc179130e618645c7
이진희 <tjrgl@naver.com>
2022-01-18 12:34:39 +0000

    Update multer.js

--- BE/utils/multer.js
+++ BE/utils/multer.js
@@ -1,22 +1,18 @@
-const multer = require("multer");
+const multer = require('multer');
 const path = require("path");
 
-const upload = multer({
-  storage: multer.diskStorage({
-    // 폴더 위치 지정
-    destination: (req, file, done) => {
-      done(null, "uploads/");
-    },
+const storage = multer.diskStorage({
+  destination: (req, file, done) => {
+    done(null, "uploads/");
+  },
+  filename: (req, file, done) => {
+    const ext = path.extname(file.originalname);
+    const fileName = path.basename(file.originalname, ext) + ext;
+    done(null, fileName);
+  },
+  limits: { fileSize: 5 * 1024 * 1024 }
+})
 
-    filename: (req, file, done) => {
-      const ext = path.extname(file.originalName);
-      // aaa.txt
-      // aaa+&&+129371271874.txt
-      const fileName = path.basename(file.originalName, ext) + Date.now() + ext;
-      done(null, fileName);
-    },
-  }),
-  limits: { fileSize: 30 * 1024 * 1024 },
-});
+const upload = multer({ storage : storage });
 
-module.exports = { upload };
+module.exports = {upload};
