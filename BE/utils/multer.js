98c7c18c4cb58444fd09a41a368811ff6e71de04
이진희 <tjrgl@naver.com>
2022-01-20 02:18:30 +0000

    Update multer.js

--- BE/utils/multer.js
+++ BE/utils/multer.js
@@ -1,9 +1,10 @@
 const multer = require('multer');
 const path = require("path");
 
+/*-------------------------------------------------------------------------*/
 const storage = multer.diskStorage({
   destination: (req, file, done) => {
-    done(null, "uploads/");
+    done(null, "uploads/logo");
   },
   filename: (req, file, done) => {
     const ext = path.extname(file.originalname);
@@ -13,6 +14,58 @@ const storage = multer.diskStorage({
   limits: { fileSize: 5 * 1024 * 1024 }
 })
 
-const upload = multer({ storage : storage });
+const logo_upload = multer({ storage : storage });
 
-module.exports = {upload};
+/*-------------------------------------------------------------------------*/
+const event_storage = multer.diskStorage({
+  destination: (req, file, done) => {
+    done(null, "uploads/event/");
+  },
+  filename : (req, file, done) => {
+    const ext = path.extname(file.originalname);
+    const fileName = path.basename(file.originalname, ext) + ext;
+    done(null, fileName);
+  },
+  limits : {
+    fileSize: 5 * 1024 * 1024
+  }
+})
+
+const event_upload = multer({storage : event_storage});
+
+/*-------------------------------------------------------------------------*/
+const service_storage = multer.diskStorage({
+  destination: (req, file, done) => {
+    done(null, "uploads/service/");
+  },
+  filename : (req, file, done) => {
+    const ext = path.extname(file.originalname);
+    const fileName = path.basename(file.originalname, ext) + ext;
+    done(null, fileName);
+  },
+  limits : {
+    fileSize: 5 * 1024 * 1024
+  }
+})
+
+const service_upload = multer({storage : service_storage});
+
+/*-------------------------------------------------------------------------*/
+const notice_storage = multer.diskStorage({
+  destination: (req, file, done) => {
+    done(null, "uploads/notice/");
+  },
+  filename : (req, file, done) => {
+    const ext = path.extname(file.originalname);
+    const fileName = path.basename(file.originalname, ext) + ext;
+    done(null, fileName);
+  },
+  limits : {
+    fileSize: 5 * 1024 * 1024
+  }
+})
+
+const notice_upload = multer({storage : notice_storage});
+
+
+module.exports = {logo_upload, event_upload, service_storage, notice_storage};
