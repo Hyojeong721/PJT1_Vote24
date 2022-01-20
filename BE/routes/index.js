9207321bba7b9d982e3226bbfc35d6dbcf0201e8
이진희 <tjrgl@naver.com>
2022-01-20 02:20:55 +0000

    Update index.js

--- BE/routes/index.js
+++ BE/routes/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const mainPage_userRouter = require("./main");
+const mainPage_userRouter = require("./mainPage");
 const hospital_userRouter = require("./hospital_user/join");
 const hospital_userRouter_login = require("./hospital_user/login");
 const duplicate_checkRouter = require("./hospital_user/duplicate_check");
 
 router.use("/api", hospital_userRouter);
-router.use("/api", hospital_userRouter_login);
+router.use("/api", mainPage_userRouter);
 router.use("/api", duplicate_checkRouter);
 router.use("/api", mainPage_userRouter);
 
