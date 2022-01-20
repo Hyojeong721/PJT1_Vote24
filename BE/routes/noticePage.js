03e8d0dcba2e9820a0a3c05c2a46d3a8ecd76ae8
이진희 <tjrgl@naver.com>
2022-01-20 10:56:09 +0000

    재수정

--- BE/routes/noticePage.js
+++ BE/routes/noticePage.js
@@ -1,54 +1,189 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
+
+const { pool } = require('../utils/mysql');
+const { logger } = require('../utils/winston');
+const { notice_upload } = require('../utils/multer');
+const { nameParser } = require('../utils/nameParser');
+
 const router = express.Router();
-const { hashPassword, comparePassword } = require("../../utils/bcrypt");
-const jwt = require("jsonwebtoken");
-const { verifyToken } = require("../../utils/jwt");
 
-const { pool } = require("../../utils/mysql");
-const { logger } = require("../../utils/winston");
+/*----------------------------------------------------------------------*
+ * GET Notice List
+ * Example URL = ../notice/947780?page=1
+ *----------------------------------------------------------------------*/
+router.get('/notice/:hospital_id', async (req, res) => {
+    try {
+        const hospital_id = req.params.hospital_id;
+        const { page } = req.query;
+        console.log(typeof(hospital_id));
+        const sql = `select 
+                        created_at, 
+                        title, 
+                        views 
+                        from hospital_notice where hospital_id =?`
 
-router.post("/login", async (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-
-  console.log(email, password);
-
-  try {
-    const sql = `SELECT id, email, password FROM hospital_info WHERE email=?`;
-    const data = await pool.query(sql, [email]);
-
-    if (data[0][0]) {
-      const UserId = data[0][0].id;
-      const hashedPassword = await hashPassword(data[0][0].password);
-      // console.log("password: [" + password + "]");
-      // console.log("hashedPassword : [" + hashedPassword + "]");
-      // const compareResult = await comparePassword(password, "ssafypw1!");
-      const compareResult = await comparePassword(password, hashedPassword);
-
-      if (compareResult) {
-        const token = jwt.sign(
-          {
-            id: UserId,
-          },
-          "ssafy",
-          { expiresIn: "24h" }
-        );
-        // console.log(token);
-        logger.info("[INFO] POST /login");
-        return res.json({ result: "ok", token: token });
-      } else {
-        // console.log(compareResult);
-        logger.info("[INFO] POST /login");
-        return res.json({ result: "인증키 발급실패 : 비밀번호를 확인해 주세요." });
-      }
-    } else {
-      logger.info("[INFO] POST /login");
-      return res.json({ result: "인증키 발급실패 : 존재하지 않는 아이디 입니다." });
+        const data = await pool.query(sql, [hospital_id]);
+        const result = data[0].slice(((page-1) * 10), (page * 10));
+        
+        logger.info('GET Notice List');
+        return res.json(result);
     }
-  } catch (error) {
-    logger.error("POST /login Error" + error);
-    console.log("POST /login Error" + error);
-    return res.json(error);
-  }
+    catch(error){
+        logger.error('GET Notice List Fail ' + error);
+        return res.json({state:'Fail'});
+    }
 });
+
+/*----------------------------------------------------------------------*
+ * GET Notice Detail
+ * Example URL = ../notice/947780/1
+ *----------------------------------------------------------------------*/
+router.get("/notice/:hospital_id/:id", async (req, res) => {
+    try {
+        const {
+            hospital_id,
+            id
+        } = req.params;
+        
+        const sql = `select * from hospital_notice where hospital_id =? and id=?`
+        const data = await pool.query(sql, [hospital_id, id]);
+        const result = data[0];
+
+        logger.info('GET Notice Detail');
+        return res.json(result);
+    }   
+    catch(error){
+        logger.error('GET Notice Detail ' + error);
+        return res.json({state:'Fail'});
+    }
+});
+
+/*----------------------------------------------------------------------*
+ * POST Notice Detail
+ * Example URL = ../notice/947780
+ *----------------------------------------------------------------------*/
+router.post('/notice/:hospital_id', notice_upload.single('notice_image'), async (req, res) => {
+    try {
+        const { 
+            hospital_id 
+        } = req.params;
+        
+        const {
+            title,
+            end_at,
+            start_at,
+            context,
+            attachment
+        } = req.body;
+
+        const rename = Date() + attachment;
+        const path = 'uploads/notice/' + rename;
+        nameParser("uploads/notice", "uploads/notice", attachment, rename);
+
+        const sql = `insert into hospital_notice (
+                        hospital_id, 
+                        title, 
+                        context, 
+                        start_at, 
+                        end_at, 
+                        attachment) value(?,?,?,?,?,?)`;
+        const data = await pool.query(sql, [hospital_id, title, context, start_at, end_at, path]);
+
+        logger.info('POST Notice Detail');
+        return res.json({state:'Success'});
+    }
+    catch(error){
+        logger.error('POST Notice Detail ' + error);
+        return res.json({state:'Fail'});
+    }
+});
+
+/*----------------------------------------------------------------------*
+ * DELETE Notice Detail
+ * Example URL = ../notice/947780/1
+ *----------------------------------------------------------------------*/
+router.delete("/notice/:hospital_id/:id", async (req, res) => {
+    try {
+        const { 
+            hospital_id, 
+            id 
+        } = req.params;
+
+        const sql = `DELETE FROM hospital_notice WHERE hospital_id =? AND id =?`
+        const data = await pool.query(sql, [hospital_id, id]);
+
+        logger.info("DELETE Notice Detail ")
+        return res.json({status:'Success'});
+    }
+    catch(error){
+        logger.error('DELETE Notice Detail ' + error);
+        return res.json({state:'Fail'});
+    }
+});
+
+/*----------------------------------------------------------------------*
+ * UPDATE Notice Detail
+ * Example URL = ../notice/947780/1
+ *----------------------------------------------------------------------*/
+router.put('/notice/:hospital_id/:id', notice_upload.single('notice_image'),async (req, res) => {
+    const {
+        hospital_id,
+        id 
+    }= req.query;
+
+    const {
+        title,
+        end_at,
+        start_at,
+        context,
+        attachment
+    } = req.body;
+
+    const rename = Date() + attachment;
+    const path = 'uploads/notice/' + rename;
+    nameParser("uploads/notice", "uploads/notice", attachment, rename);
+
+    try {
+        const sql = `update hospital_notice set
+            title =?, 
+            context =?, 
+            start_at =?, 
+            end_at =?, 
+            attachment =? where id=? AND hospital_id`;
+        const data = await pool.query(sql, [title, context, start_at, end_at, attachment, id, hospital_id]);
+
+        logger.info('UPDATE Notice Detail');
+        return res.json({state:'Success'});
+    }
+    catch(error){
+        logger.error('UPDATE Notice Detail ' + error);
+        return res.json({state:'Fail'});
+    }    
+});
+
+/*----------------------------------------------------------------------*
+ * GET File Download
+ * Example URL = ../notice/947780/3/download
+ *----------------------------------------------------------------------*/
+router.get("/notice/:hospital_id/:id/download", async (req, res) => {
+    const {
+        hospital_id,
+        id
+    } = req.params;
+
+    try {  
+        const sql = `SELECT attachment FROM hospital_notice WHERE hospital_id =? AND id=?`;
+        const data = await pool.query(sql, [hospital_id, id]);
+        const filepath = data[0][0].attachment;
+
+        logger.info('GET File Download');
+        return res.download(path.join(__dirname, filepath));
+    }
+    catch(error){
+        logger.error('GET File Download ' + error);
+        return res.json({state: 'Fail'});
+    }
+})
+
 module.exports = router;
