98b252b8b67f8fb4bb514a77a6ef6f18f1a00322
hyojeong <ase0574@naver.com>
2022-01-21 17:30:16 +0900

    feat: postdetail 서버 연결 수정중

---
+++ FE/components/PostDetail.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import axios from "axios";
+
+function PostDetail() {
+  const [postdata, setPostData] = useState([]);
+  const router = useRouter();
+  const { id } = router.query;
+
+  console.log(id);
+
+  const getPost = async () => {
+    const res = await axios.get(
+      `http://teama205.iptime.org/api/event/947780/${id}`
+    );
+    console.log(res);
+    const data = res.data;
+    console.log(data);
+    setPostData(data);
+  };
+  getPost();
+
+  return (
+    <div>
+      Post id
+      {postdata}
+    </div>
+  );
+}
+
+export default PostDetail;
