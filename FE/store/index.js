936e9aafcd4b07593ef191e0b85d3d9e0f7fa78e
j1h00 <guruboy@snu.ac.kr>
2022-01-22 15:44:26 +0900

    chores: edit line breaks

--- FE/store/index.js
+++ FE/store/index.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { createWrapper } from "next-redux-wrapper";
 import { persistStore } from "redux-persist";
-
 import logger from "redux-logger";
 import { rootReducer, persistedReducer } from "./modules";
 
