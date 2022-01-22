176256d98dde54b78c2f66aa9efe5982e5480286
hyojeong <ase0574@naver.com>
2022-01-23 01:23:13 +0900

    feat: event list page, create pagge 작성

--- FE/pages/event/index.js
+++ FE/pages/event/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "../../components/Header";
-import PostList from "../../components/PostList";
+import EventList from "../../components/EventList";
 
 function HospitalEvent() {
   return (
     <div>
       <Header title="병원 이벤트"></Header>
       <div className="container">
-        <PostList></PostList>
+        <EventList></EventList>
       </div>
     </div>
   );
