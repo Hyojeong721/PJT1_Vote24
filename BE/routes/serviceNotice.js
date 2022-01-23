68dcf1d9dea31203c0382f638565b3b29ddbede5
이진희 <tjrgl@naver.com>
2022-01-23 15:43:52 +0000

    Update serviceNotice.js

--- BE/routes/serviceNotice.js
+++ BE/routes/serviceNotice.js
@@ -9,58 +9,92 @@ const { nameParser } = require("../utils/nameParser");
 
 const router = express.Router();
 
-// service_notice write.
+/*----------------------------------------------------------------------*
+ * POST Service Notice Detail
+ * Example URL = ../service
+ *----------------------------------------------------------------------*/
 router.post("/service", service_upload.single("service_notice_image"), async (req, res) => {
-  const { title, context, fixed, attachment } = req.body;
+  const { 
+      title, 
+      context, 
+      fixed, 
+      attachment 
+  } = req.body;
+
   const rename = Date() + attachment;
   const path = "uploads/service/" + rename;
 
   try {
     if (req.body.attachment) {
       nameParser("uploads/service", "uploads/service", attachment, rename);
-      const sql = `INSERT INTO service_notice ( title, context, fixed, attachment) VALUES(?, ?, ?, ?);`;
+      const sql = `INSERT INTO service_notice ( 
+                        title, 
+                        context, 
+                        fixed, 
+                        attachment) VALUES(?, ?, ?, ?);`;
       const data = await pool.query(sql, [title, context, fixed, path]);
-    } else {
-      const sql = `INSERT INTO service_notice ( title, context, fixed) VALUES(?, ?, ?);`;
+    } 
+    else {
+      const sql = `INSERT INTO service_notice ( 
+                        title, 
+                        context, 
+                        fixed) VALUES(?, ?, ?);`;
       const data = await pool.query(sql, [title, context, fixed]);
     }
-    logger.info("[INFO] POST /service_notice/write");
-    return res.json({ result: "ok" });
+    logger.info("POST Service Notice Detail");
+    return res.json({ state: "Success" });
   } catch (error) {
-    logger.error("POST /insert Error" + error);
-    return res.json(error);
+    logger.error("POST Service Notice Detail " + error);
+    return res.json({state: "Fail"});
   }
 });
 
-// service_notice update.
-
-// params로 id 받는 방식
-// router.put("/:id", upload.single("attachment"), async (req, res) => {
-// const id = req.params.id;
-// json객체로 id 받는 방식
+/*----------------------------------------------------------------------*
+ * PUT Service Notice Detail
+ * Example URL = ../service/1
+ *----------------------------------------------------------------------*/
 router.put("/service/:id", service_upload.single("service_notice_image"), async (req, res) => {
   const id = req.params.id;
-  const { title, context, fixed, attachment } = req.body;
+  const { 
+    title, 
+    context, 
+    fixed, 
+    attachment 
+  } = req.body;
+
   const rename = Date() + attachment;
   const path = "uploads/service/" + rename;
+
   try {
     if (req.body.attachment) {
       nameParser("uploads/service", "uploads/service", attachment, rename);
-      const sql = `UPDATE service_notice SET title=?, context=?, fixed=?, attachment=? WHERE id=?;`;
+      const sql = `UPDATE service_notice SET 
+                      title=?, 
+                      context=?, 
+                      fixed=?, 
+                      attachment=? WHERE id=?;`;
       const data = await pool.query(sql, [title, context, fixed, path, id]);
-    } else {
-      const sql = `UPDATE service_notice SET title=?, context=?, fixed=? WHERE id=?;`;
+    } 
+    else {
+      const sql = `UPDATE service_notice SET 
+                      title=?, 
+                      context=?, 
+                      fixed=? WHERE id=?;`;
       const data = await pool.query(sql, [title, context, fixed, id]);
     }
-    logger.info("[INFO] PATCH /service_notice/update");
-    return res.json({ result: "ok" });
+
+    logger.info("PUT Service Notice Detail");
+    return res.json({ result: "Success" });
   } catch (error) {
-    logger.error("POST /update Error" + error);
+    logger.error("PUT Service Notice Detail " + error);
     return res.json(error);
   }
 });
 
-// service_notice delete
+/*----------------------------------------------------------------------*
+ * DELETE Service Notice Detail
+ * Example URL = ../service/1
+ *----------------------------------------------------------------------*/
 router.delete("/service/:id", async (req, res) => {
   const id = req.params.id;
 
@@ -68,15 +102,18 @@ router.delete("/service/:id", async (req, res) => {
     const sql = `DELETE FROM service_notice WHERE id=?;`;
     const data = await pool.query(sql, [id]);
 
-    logger.info("[INFO] DELETE /service_notice/delete");
-    return res.json({ result: "ok" });
+    logger.info("DELETE Service Notice Detail");
+    return res.json({ result: "Success" });
   } catch (error) {
-    logger.error("GET /delete Error" + error);
+    logger.error("DELETE Service Notice Detail " + error);
     return res.json(error);
   }
 });
 
-// service_notice Detail
+/*----------------------------------------------------------------------*
+ * GET Service Notice Detail
+ * Example URL = ../service/1
+ *----------------------------------------------------------------------*/
 router.get("/service/:id", async (req, res) => {
   const id = req.params.id;
 
@@ -85,36 +122,41 @@ router.get("/service/:id", async (req, res) => {
     const data = await pool.query(sql, [id]);
     const result = data[0];
 
-    // 첨부 파일은 어떻게 보내야할까..?
-
-    logger.info("[INFO] GET /service_notice/detail");
+    logger.info("GET Service Notice Detail");
     return res.json(result);
-  } catch (error) {
-    logger.error("GET /select Error" + error);
+  } 
+  catch (error) {
+    logger.error("GET Service Notice Detail " + error);
     return res.json(error);
   }
 });
 
-// service_notice list
-
-router.get("/service", async (req, res) => {
+/*----------------------------------------------------------------------*
+ * GET Service Notice List
+ * Example URL = ../service/1
+ *----------------------------------------------------------------------*/
+router.get("/service/", async (req, res) => {
   try {
     // front에서 필요한 부분만 보내도록 쿼리 수정가능
 
-    // const { page } = req.query;
+    const { page } = req.query;
     const sql = `SELECT * FROM service_notice;`;
     const data = await pool.query(sql);
-    // const result = data[0].slice((page - 1) * 10, page * 10);
-    const result = data[0];
+    const result = data[0].slice((page - 1) * 10, page * 10);
 
-    logger.info("[INFO] GET /service_notice/list");
+    logger.info("GET Service Notice List");
     return res.json(result);
-  } catch (error) {
-    logger.error("GET /select Error" + error);
+  } 
+  catch (error) {
+    logger.error("GET Service Notice List " + error);
     return res.json(error);
   }
 });
 
+/*----------------------------------------------------------------------*
+ * GET Service File Download
+ * Example URL = ../service/3/download
+ *----------------------------------------------------------------------*/
 router.get("/service/:id/download", async (req, res) => {
   const id = req.params.id;
 
@@ -123,13 +165,12 @@ router.get("/service/:id/download", async (req, res) => {
     const data = await pool.query(sql, [id]);
     const filepath = data[0][0].attachment;
 
-    console.log(filepath);
-
-    logger.info("GET /service_notice/:id/download Success");
+    logger.info("GET Service File Download");
     return res.download(path.join(__dirname, filepath));
-  } catch (error) {
-    logger.error("GET Fail " + error);
-    return res.send("Fail");
+  } 
+  catch (error) {
+    logger.error("GET Service File Download " + error);
+    return res.send({state:"Fail"});
   }
 });
 
