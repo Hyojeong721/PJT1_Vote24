52997966df60839acf7cd880e00b8a44f149eff7
j1h00 <guruboy@snu.ac.kr>
2022-01-26 17:12:27 +0900

    feat: Edit user page url

--- FE/pages/user/[hId]/index.js
+++ FE/pages/user/[hId]/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-function HomeUser() {
+function HomeUser({ name, logo_file, phone }) {
   return (
     <div>
       <div className="background bg-primary d-flex justify-content-center align-items-center">
         <div className="main_body d-flex flex-column justify-content-between align-items-center">
           <div className="main_title text-center text-white fw-bold ">
-            <div>Vote24</div>
-            <div>설문조사 플랫폼</div>
+            <div>name</div>
+            <div>phone</div>
+            <div>logo_file</div>
           </div>
         </div>
       </div>
@@ -19,12 +20,13 @@ export default HomeUser;
 export async function getServerSideProps({ params }) {
   console.log(params);
   const GET_HOSPITAL_INFO_URL = `http://i6a205.p.ssafy.io:8000/api/code/${params.hId}`;
-  const { name, file } = await axios.get(GET_HOSPITAL_INFO_URL);
+  const { name, phone, logo_file } = await axios.get(GET_HOSPITAL_INFO_URL);
 
   return {
     props: {
-      name: "name",
-      file: "file",
+      name,
+      logo_file,
+      phone,
     },
   };
 }
