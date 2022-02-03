96e345be184694d195f7334482f22ea83d9b9228
hyojeong <ase0574@naver.com>
2022-02-03 10:40:47 +0900

    feat: Add service index page, detail page

--- FE/pages/service/notice/[id].js
+++ FE/pages/service/notice/[id].js
@@ -1,51 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import { getPostByNo } from "../../../data/Postdata";
+import React from "react";
+import Header from "../../../components/Header";
+import NoticeDetailItem from "../../../components/Notice/NoticeDetailItem";
 
-const NoticeDetail = ({ history, location, match }) => {
-  const [id, setId] = useState("");
-  const [data, setData] = useState({});
-  const router = useRouter();
-  setId(router.query.id);
-
-  useEffect(() => {
-    setData(getPostByNo(1));
-  }, []);
-
+function NoticeDetail() {
   return (
     <div>
-      <div className="post-view-wrapper">
-        {data ? (
-          <div>
-            <div className="post-view-row">
-              <label>게시글 번호</label>
-              <label>{data.no}</label>
-            </div>
-            <div className="post-view-row">
-              <label>제목</label>
-              <label>{data.title}</label>
-            </div>
-            <div className="post-view-row">
-              <label>작성일</label>
-              <label>{data.createData}</label>
-            </div>
-            <div className="post-view-row">
-              <label>내용</label>
-              <div>{data.content}</div>
-            </div>
-          </div>
-        ) : (
-          "해당 게시글을 찾을 수 없습니다."
-        )}
-        <button
-          className="post-view-go-list-btn"
-          onClick={() => history.goBack()}
-        >
-          목록으로 돌아가기
-        </button>
+      <Header title="Vote24 공지사항"></Header>
+      <div className="container">
+        <NoticeDetailItem></NoticeDetailItem>
       </div>
     </div>
   );
-};
+}
 
 export default NoticeDetail;
--- FE/pages/service/notice/index.js
+++ FE/pages/service/notice/index.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../../components/Header";
+import axios from "axios";
+import NoticeList from "../../../components/Notice/NoticeList";
+import Paging from "../../../components/Paging";
 
-function NoticeHospital() {
+const NOTICE_URL = `http://i6a205.p.ssafy.io:8000/api/service/`;
+
+function HospitalEvent() {
+  const [dataList, setDataList] = useState([]);
+  // 페이징 처리를 위한
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
+
+  // 서버에서 공지 목록 받아오는 코드
+  useEffect(() => {
+    const getList = async () => {
+      const res = await axios.get(`${NOTICE_URL}`);
+      const data = res.data;
+      console.log(data);
+      setDataList(data);
+    };
+    getList();
+  }, []);
+
+  // 페이징 처리를 위한 계산
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = dataList.slice(indexOfFirstPost, indexOfLastPost);
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
-    <>
-      <Header title="서비스 공지사항"></Header>
-      <div className="container"></div>
-    </>
+    <div>
+      <Header title="Vote24 공지사항"></Header>
+      <div className="container mt-3">
+        <NoticeList dataList={currentPosts} />
+
+        <Paging
+          postsPerPage={postsPerPage}
+          totalPosts={dataList.length}
+          paginate={paginate}
+        />
+      </div>
+    </div>
   );
 }
 
-export default NoticeHospital;
+export default HospitalEvent;
