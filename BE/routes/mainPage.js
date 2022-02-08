0843ec4795f80e104f629ed48176d98aff43591a
김종범 <rlawhd011@naver.com>
2022-02-08 14:40:16 +0900

    설문 결과, 수정 에러 디버깅

--- BE/routes/mainPage.js
+++ BE/routes/mainPage.js
@@ -1,16 +1,70 @@
 const express = require("express");
+const fs = require("fs");
+const schedule = require("node-schedule");
 const router = express.Router();
 
 const { pool } = require("../utils/mysql");
 const { logger } = require("../utils/winston");
 const { verifyToken } = require("../utils/jwt");
+const today = new Date();
 
-router.get("/main", verifyToken, (req, res) => {
+// 매일 업데이트 되는 sum.txt 파일
+schedule.scheduleJob("0 0 0 * *", function () {
+  const total = pool.query(`SELECT SUM(count) FROM hospital_survey`);
+  console.log(total);
   try {
-    logger.info("GET '/'");
-    res.send("hello world");
-  } catch {
-    logger.error("GET '/ Error" + error);
+    fs.writeFile("../data/sum.txt", `${total[0]}`);
+    logger.info("Update Total Count Success");
+  } catch (error) {
+    logger.error("Update Total Count Fail " + error);
+  }
+});
+
+/*----------------------------------------------------------------------*
+ * GET Main Detail
+ * Example URL = ../main
+ *----------------------------------------------------------------------*/
+router.get("/main/:hospital_id", async (req, res) => {
+  const hospital_id = req.params.hospital_id;
+
+  const date = `'${today.getFullYear()}-${("0" + today.getMonth() + 1).slice(-2)}-${(
+    "0" +
+    (today.getDate() + 1)
+  ).slice(-2)}'`;
+  try {
+    const available_eventCnt = await pool.query(
+      `SELECT COUNT(id) AS cnt FROM hospital_event WHERE start_at <= ${date} AND end_at > ${date}`
+    );
+    const available_surveyCnt = await pool.query(
+      `SELECT COUNT(id) AS cnt FROM hospital_survey WHERE start_at <= ${date} AND end_at > ${date}`
+    );
+
+    const totalVote = await pool.query(
+      `SELECT SUM(count) AS cnt FROM hospital_survey WHERE start_at <= ${date}`
+    );
+    if (totalVote[0][0].cnt == null) {
+      totalVote[0][0].cnt = 0;
+    }
+
+    const lastVote = fs.readFileSync("data/sum.txt", "utf-8");
+    const todayVote = totalVote[0][0].cnt - lastVote;
+
+    const populartVotes = await pool.query(
+      `SELECT title, count FROM hospital_survey WHERE hospital_id = ${hospital_id} AND start_at <= ${date} AND end_at > ${date} ORDER BY count DESC`
+    );
+
+    const result = {
+      available_eventCnt: available_eventCnt[0][0].cnt,
+      available_surveyCnt: available_surveyCnt[0][0].cnt,
+      totalVote: totalVote[0][0].cnt,
+      todayVote: todayVote,
+      populartVotes: populartVotes[0],
+    };
+
+    logger.info("GET Main Success");
+    res.send(result);
+  } catch (error) {
+    logger.error("GET Main Fail" + error);
     return res.json(error);
   }
 });
