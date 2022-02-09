b8d6693f00ebf49c80083f08be10cf9e92f24798
j1h00 <guruboy@snu.ac.kr>
2022-02-09 11:04:03 +0900

    feat: Edit mainOnLogin Header

--- FE/components/Header.js
+++ FE/components/Header.js
@@ -2,10 +2,12 @@ function Header({ title, subtitle, children }) {
   return (
     <div className="header-box bg-primary text-white d-flex flex-column justify-content-center">
       <div className="header-title ms-3">{title}</div>
-      <div className="fs-2 ms-3 d-flex">
-        <p>{subtitle}</p>
-        {children}
-      </div>
+      {children && (
+        <div className="fs-2 ms-3 d-flex mt-3">
+          <p>{subtitle}</p>
+          {children}
+        </div>
+      )}
     </div>
   );
 }
