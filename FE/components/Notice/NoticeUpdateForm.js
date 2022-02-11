6d805ee16ef4b536cfb0e7d381fa62c36a3a5fbe
hyojeong <ase0574@naver.com>
2022-02-11 13:58:59 +0900

    feat: edit notice update page

--- FE/components/Notice/NoticeUpdateForm.js
+++ FE/components/Notice/NoticeUpdateForm.js
@@ -10,7 +10,7 @@ import cs from "../../styles/noticecreate.module.css";
 const NoticeUpdateForm = ({ noticeId, url }) => {
   const [values, setValues] = useState([]);
   const router = useRouter();
-
+  // 기존 data 가져오기
   useEffect(() => {
     const getPost = async () => {
       const res = await axios.get(`${url}/`);
@@ -31,17 +31,16 @@ const NoticeUpdateForm = ({ noticeId, url }) => {
       [name]: value,
     }));
   };
+
   // 글 수정 서버 요청
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(values);
     if (values.title == "") {
       alert("제목을 입력하세요.");
     } else if (values.context == "") {
       alert("내용을 입력하세요.");
     } else {
-      console.log("success");
-      // 보낼 데이터들을 fromdata에 담아
+      console.log("수정 성공");
       const fd = new FormData();
       for (let key in values) {
         if (key === "imgFile") {
@@ -75,77 +74,95 @@ const NoticeUpdateForm = ({ noticeId, url }) => {
             autoClose: 3000,
           });
           console.log(err);
-          console.log(url, fd);
         });
     }
   };
   // 고정 체크 박스
-  const handleCheckChange = () => {
-    if (document.getElementById("input_check").checked) {
-      handleChange("fixed", "1");
-    } else {
-      handleChange("fixed", "0");
-    }
+  const handlefixed = (e) => {
+    console.log(e.target);
+    handleChange("fixed", e.target.value);
   };
 
   return (
-    <form onSubmit={handleSubmit} className={cn(cs.noticeCreate)}>
-      <div>
-        <p>
-          <label className="form-label">
+    <form onSubmit={handleSubmit}>
+      <div name="form" className={cn(cs.form)}>
+        <div name="제목" className={cn(cs.formRow, "d-flex")}>
+          <div className={cn(cs.formLabel)}>
+            <label htmlFor="title">
+              <span className={cn(cs.star)}>*{"  "}</span>제목
+            </label>
+          </div>
+          <div className={cn(cs.formControl)}>
             <input
-              type="checkbox"
+              className={cn(cs.input)}
+              name="title"
+              onChange={handleInputChange}
+              id="title"
+              value={values.title}
+            ></input>
+          </div>
+        </div>
+
+        <div name="체크박스" className={cn(cs.formRow, "d-flex")}>
+          <div className={cn(cs.formLabel)}>
+            <span className={cn(cs.star)}>*{"  "}</span>TYPE
+          </div>
+          <div className={cn(cs.formControl)}>
+            <span className="form-label me-3">
+              <label htmlFor="fixed" />
+              <input
+                id="fixed"
+                type="radio"
+                name="fixed"
+                defaultValue={1}
+                checked={values.fixed == 1}
+                onChange={handlefixed}
+              />
+              {"  "}
+              고정공지
+            </span>
+            <label htmlFor="no_fixed" className="form-label" />
+            <input
+              id="no_fixed"
+              type="radio"
               name="fixed"
-              value="1"
-              id="input_check"
-              onChange={handleCheckChange}
+              defaultValue={0}
+              checked={values.fixed == 0}
+              onChange={handlefixed}
             />
-            고정공지
-          </label>
-        </p>
-      </div>
-      <div className={cn(cs.formRow, cs.formRowtop, "d-flex")}>
-        <div className={cn(cs.formLabel)}>
-          <label htmlFor="title">
-            <span className={cn(cs.star)}>*{"  "}</span>제목
-          </label>
-        </div>
-        <div className={cn(cs.formControl)}>
-          <input
-            className={cn(cs.input)}
-            name="title"
-            onChange={handleInputChange}
-            id="title"
-            value={values.title}
-          ></input>
-        </div>
-      </div>
-      <div className={cn(cs.formRow, "d-flex")}>
-        <div className={cn(cs.formLabel)}>
-          <label htmlFor="context">
-            <span className={cn(cs.star)}>*{"  "}</span>내용
-          </label>
+            {"  "}
+            일반공지
+          </div>
         </div>
 
-        <div className={cn(cs.formControl)}>
-          <textarea
-            className={cn(cs.textarea)}
-            name="context"
-            value={values.context}
-            onChange={handleInputChange}
-            id="context"
-          ></textarea>
+        <div name="내용" className={cn(cs.formRow, "d-flex")}>
+          <div className={cn(cs.formLabel)}>
+            <label htmlFor="context">
+              <span className={cn(cs.star)}>*{"  "}</span>내용
+            </label>
+          </div>
+
+          <div className={cn(cs.formControl)}>
+            <textarea
+              className={cn(cs.textarea)}
+              name="context"
+              value={values.context}
+              onChange={handleInputChange}
+              id="context"
+            ></textarea>
+          </div>
+        </div>
+
+        <div name="첨부파일" className={cn(cs.formRow)}>
+          <FileInput
+            name="imgFile"
+            value={values.imgFile}
+            onChange={handleChange}
+          ></FileInput>
         </div>
       </div>
-      <div className={cn(cs.formRow)}>
-        <FileInput
-          name="imgFile"
-          value={values.imgFile}
-          onChange={handleChange}
-        ></FileInput>
-      </div>
 
-      <div className={cn(cs.btns, "d-flex")}>
+      <div name="취소등록버튼" className={cn(cs.btns, "d-flex")}>
         <div className={cn(cs.btn)}>
           <Link href="/notice/" passHref>
             <button className="btn btn-secondary">취소</button>
