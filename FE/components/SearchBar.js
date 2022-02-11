ef8b837af4a563ee071cde1d84f682fdecbe6a55
j1h00 <guruboy@snu.ac.kr>
2022-02-11 13:57:33 +0900

    feat: Edit search bar (setTimeout, enter Keydown)

--- FE/components/SearchBar.js
+++ FE/components/SearchBar.js
@@ -4,9 +4,25 @@ import styles from "../styles/searchbar.module.css";
 
 function SearchBar({ setPostList, postListProp }) {
   const [searchInput, setSearchInput] = useState("");
+  const [timer, setTimer] = useState(null);
 
   const handleOnChange = (value) => {
+    clearTimeout(timer);
     setSearchInput(value);
+    const newTimer = setTimeout(() => {
+      triggerChange(value);
+    }, 500);
+
+    setTimer(newTimer);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      triggerChange(e.target.value);
+    }
+  };
+
+  const triggerChange = (value) => {
     setPostList(postListProp.filter((p) => p.title.includes(value)));
   };
 
@@ -20,6 +36,7 @@ function SearchBar({ setPostList, postListProp }) {
           aria-label="Search"
           value={searchInput}
           onChange={(e) => handleOnChange(e.target.value)}
+          onKeyDown={(e) => handleKeyDown(e)}
         />
         <button
           className="position-absolute top-50 end-0 translate-middle btn material-icons p-0 "
