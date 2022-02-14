840d528b79c3b286e40995bdadf7eca8b6b8a5f4
hyojeong <ase0574@naver.com>
2022-02-14 17:14:12 +0900

    fix: survey_result_chart 수정중

--- FE/components/Survey/BarChart.js
+++ FE/components/Survey/BarChart.js
@@ -22,17 +22,9 @@ ChartJS.register(
 const BarChart = ({ total, item }) => {
   console.log(item);
   const options = {
-    responsive: true,
+    responsive: false,
     indexAxis: "y",
     scales: {
-      y: {
-        ticks: {
-          font: {
-            size: 20,
-            weight: "bold",
-          },
-        },
-      },
       x: {
         min: 0,
         max: 100,
@@ -66,9 +58,7 @@ const BarChart = ({ total, item }) => {
   };
 
   return (
-    <div>
-      <Bar data={data} options={options} />
-    </div>
+    <Bar id="myChart" width="100" height="100" data={data} options={options} />
   );
 };
 
