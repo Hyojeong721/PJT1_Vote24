a5d3d4d999089b5409cab11cc443f79d9cef941c
김종범 <rlawhd011@naver.com>
2022-02-18 09:23:30 +0900

    mysql 비밀번호 설정

--- BE/utils/mysql.js
+++ BE/utils/mysql.js
@@ -1,8 +1,9 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "localhost",
+  host: "172.19.0.2",
   user: "a205",
+  password: "ssafy1!",
   database: "ssafy24",
   writeforConnection: true,
   connectionLimit: 10,
